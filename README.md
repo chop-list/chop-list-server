67b219281ed72a2125287bc800b59aeea3a68617
Miguel Angel Alonso Pérez <migalons74@gmail.com>
2018-11-08 19:33:20 +0100

    Serve grahql over koajs

---
+++ README.md
@@ -0,0 +1,6 @@
+TO DO....
+
+
+
+[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg)](http://standardjs.com)
+
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-require("./lib/server");
+const server = require("./lib/server");
+const program = require('commander');
+
+//Init logger
+require('./lib/util/log');
+
+program
+    .version(require("./package.json").version)
+    .option("-p --port [port]", "Port to use", 8000)
+    .option("-v --verbose");
+
+if(program.verbose) {
+    log.level = "debug";
+}
+
+server({port: program.port});
+
